4a95995e0d978bda46428084eb147a95ccfdbbfc
Bahman Movaqar <bahman@bahmanm.com>
2023-09-27 02:35:04 -0700

    Tune the frequency/number of probe HTTP requests (#6)
    * Backup grafana.db before removing
    * Removed excess probes
    * Reconfigured timeouts and only use 2xx probes
    * Removed real live instances until admin confirmation
    * Revamped dashboard design
    * Install CSV data source plugin and back up Grafana DB
    
    * Revamped dashboard design
    
    - The landing page only contains the overall health gauges.
    - The gauges have data links which enable the user to drill down and
    see details per instance.

--- mk/lemmy-meter.mk
+++ mk/lemmy-meter.mk
@@ -18,9 +18,12 @@ lemmy-meter..deploy-root = $(DEPLOY_ROOT)lemmy-meter/
 lemmy-meter..src-docker-compose.yml := $(ROOT)docker/docker-compose.yml
 lemmy-meter..docker-compose.yml = $(lemmy-meter..deploy-root)docker-compose.yml
 
+lemmy-meter..ts = $(shell date '+%Y%m%d-%H%M%S')
+
 lemmy-meter..grafana-volume := $(lemmy-meter..deploy-root)volumes/grafana
 lemmy-meter..src-grafana.db := $(ROOT)config/grafana.db
 lemmy-meter..grafana.db = $(lemmy-meter..deploy-root)volumes/grafana/grafana.db
+lemmy-meter..grafana.db.backup = $(lemmy-meter..src-grafana.db).$(lemmy-meter..ts).backup
 
 ####################################################################################################
 
@@ -72,6 +75,7 @@ lemmy-meter.up : lemmy-meter..ensure-variables \
 		bmakelib.default-if-blank( lemmy-meter.project-name,lemmy-meter ) \
 		$(lemmy-meter..docker-compose.yml) \
 		lemmy-meter..volumes \
+		lemmy-meter.grafana-db.backup \
 		$(lemmy-meter..grafana.db)
 	export UID \
 	&& export GID=$$(id -g) \
@@ -122,3 +126,40 @@ lemmy-meter.restart-% : lemmy-meter..ensure-variables \
 		--project-directory $(lemmy-meter..deploy-root) \
 		restart \
 		$(*)
+
+
+####################################################################################################
+
+.PHONY : lemmy-meter.grafana-db.backup
+
+lemmy-meter.grafana-db.backup :		$(lemmy-meter..grafana.db.backup)
+
+####################################################################################################
+
+$(lemmy-meter..grafana.db.backup) : $(lemmy-meter..grafana.db)
+	-cp $(<) $(@)
+
+
+####################################################################################################
+
+grafana..configure :	\
+		bmakelib.default-if-blank( admin-password,admin ) \
+		bmakelib.default-if-blank( lemmy-meter.project-name,lemmy-meter )
+	export UID \
+	&& export GID=$$(id -g) \
+	&& cd $(lemmy-meter..deploy-root) \
+	&& docker compose exec \
+		--detach \
+		grafana \
+		grafana-cli \
+			plugins \
+			install \
+			marcusolsson-csv-datasource
+	&& docker compose exec \
+		--detach \
+		grafana \
+		grafana-cli \
+			--homepath '/var/lib/grafana' \
+			admin \
+			reset-admin-password \
+			$(admin-password)
